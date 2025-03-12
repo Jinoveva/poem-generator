e33a0320a7b2591531818caf028e1e2467ba3cf1
Jinoveva <jinovevalopes@gmail.com>
2025-03-12 19:42:33 +0200

    Finished project

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${name.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${name.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
