cb7a8387c198dd9a532d6880db50c81f62dd41ff
Jinoveva <jinovevalopes@gmail.com>
2025-03-12 19:41:38 +0200

    Added blink animation

--- index.js
+++ index.js
@@ -14,8 +14,12 @@ function generatePoem(event) {
   let apiKey = "ft01o336fa01b0d041f3cbcd1c5dc250";
   let prompt = `Generate a poem about ${name.value}`;
   let context =
-    "You are a poet who is skilled at generating cool poems just by using a person's name. You duty is to generate a 4 line poem more life a motivational one based on one's name. Sign it at the end with your inner self with a <straong> after a <br/> tag";
+    "You are a poet who is skilled at generating cool poems just by using a person's name. You duty is to generate a 4 line poem more life a motivational one based on one's name. Sign it at the end with your inner self with a <strong> after a <br/> tag";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${name.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
